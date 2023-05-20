445359a28bc9fb25f12fc54d0064cc86ca10c716
M.Mahad <mahadmuhammad0332@gmail.com>
2023-05-20 15:59:58 +0500

    Update app.js

--- Js Assignments/Assignments/Assignment-1/app.js
+++ Js Assignments/Assignments/Assignment-1/app.js
@@ -1,2 +1,5 @@
-alert ("Welcome to my website")
+alert ("Welcome to my website");
+alert ("Error! PLease enter a valid password");
+alert("Welcome to JS land....\nHappy Coding!");
+console.log("Hello...I can run JS through my web browser's console");
 
